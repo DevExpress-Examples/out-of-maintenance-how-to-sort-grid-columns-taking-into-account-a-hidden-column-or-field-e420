ad56e4d2d67010ab817ef32bf90811719ab02c76
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to sort grid columns taking into account a hidden column or field
+
+
+<p>Sometimes you need to keep an internal grouping when a user sorts against grid columns. Assume that you have a list of products. Every product belongs to a particular category.  A user is able to sort it against the product name and product ID. However, it's necessary to keep an internal sort order against categories. <br />
+It can be easily implemented using the GridView.CustomColumnSort event.</p>
+
+<br/>
+
+
---
+++ VB/WindowsApplication9/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("WindowsApplication9")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WindowsApplication9")>
+<Assembly: AssemblyCopyright("Copyright ©  2008")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("449fcb98-0a91-4d9e-914c-7cf3b5198199")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
